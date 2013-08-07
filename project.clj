b21bd47cdd5525c25c2e1b7184358fd17e0edc3e
Steve Shogren <steve.a.shogren@gmail.com>
2013-08-07 19:20:40 -0400

    Fixing the url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject deft "0.1.0"
+(defproject deft "0.1.1"
   :description "A library for adding a basic runtime 'type-shape' checking to clojure functions"
-  :url "www.github.com/steveshogren/deft"
+  :url "http://www.github.com/steveshogren/deft"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
