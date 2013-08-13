58935a7af00b3c5e2e4106e94a63de517a86b2d4
Steve Shogren <steve.a.shogren@gmail.com>
2013-08-13 16:51:51 -0400

    Allowing defrecords as a typeshape

--- src/deft/core.clj
+++ src/deft/core.clj
@@ -7,11 +7,13 @@
   (symbol (str "auto_" (rand-int 10000000))))
 
 (defn is-type [coll# type#]
-  (reduce (fn [iret# k#]
-            (and iret#
-                 (contains? coll# k#)))
-          true
-          type#))
+  (if (vector? type#) 
+    (reduce (fn [iret# k#]
+              (and iret#
+                   (contains? coll# k#)))
+            true
+            type#)
+    (instance? type# coll#)))
 
 (defmacro deft [name# & rest#]
   "deft [name doc? [param Type*] Type body]
